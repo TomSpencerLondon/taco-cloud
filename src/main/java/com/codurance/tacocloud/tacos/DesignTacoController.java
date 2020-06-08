42b90de398dd66493e4fe738307f4e848025c494
Tom <thomas.spencer@codurance.com>
2020-06-08 18:56:42 +0100

    Add H2 console database

--- src/main/java/com/codurance/tacocloud/tacos/DesignTacoController.java
+++ src/main/java/com/codurance/tacocloud/tacos/DesignTacoController.java
@@ -1,10 +1,13 @@
 package com.codurance.tacocloud.tacos;
 
 import com.codurance.tacocloud.tacos.Ingredient.Type;
+import com.codurance.tacocloud.tacos.data.IngredientRepository;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,20 +19,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/design")
 public class DesignTacoController {
 
+  private final IngredientRepository ingredientRepo;
+
+  @Autowired
+  public DesignTacoController(IngredientRepository ingredientRepo) {
+    this.ingredientRepo = ingredientRepo;
+  }
+
   @GetMapping
   public String showDesignForm(Model model) {
-    List<Ingredient> ingredients = Arrays.asList(
-        new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-        new Ingredient("COTO", "Ground Tortilla", Type.WRAP),
-        new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-        new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-        new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-        new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-        new Ingredient("CHED", "Cheddar", Type.CHEESE),
-        new Ingredient("JACK", "Monterray Jack", Type.CHEESE),
-        new Ingredient("SLSA", "Salsa", Type.SAUCE),
-        new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-    );
+    List<Ingredient> ingredients = new ArrayList<>();
+    ingredientRepo.findAll().forEach(i -> ingredients.add(i));
 
     Type[] types = Ingredient.Type.values();
     for (Type type : types) {
--- src/main/java/com/codurance/tacocloud/tacos/data/JdbcIngredientRepository.java
+++ src/main/java/com/codurance/tacocloud/tacos/data/JdbcIngredientRepository.java
@@ -1,6 +1,8 @@
 package com.codurance.tacocloud.tacos.data;
 
 import com.codurance.tacocloud.tacos.Ingredient;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -16,16 +18,30 @@ public class JdbcIngredientRepository implements IngredientRepository {
 
   @Override
   public Iterable<Ingredient> findAll() {
-    return null;
+    return jdbc.query("select id, name, type from Ingredient", this::mapRowToIngredient);
+  }
+
+  private Ingredient mapRowToIngredient(ResultSet resultSet, int rowNum) throws SQLException {
+    return new Ingredient(
+        resultSet.getString("id"),
+        resultSet.getString("name"),
+        Ingredient.Type.valueOf(resultSet.getString("type"))
+    );
   }
 
   @Override
   public Ingredient findOne(String id) {
-    return null;
+    return jdbc.queryForObject("select id, name, type from Ingredient where id=?",
+        this::mapRowToIngredient, id);
   }
 
   @Override
   public Ingredient save(Ingredient ingredient) {
-    return null;
+    jdbc.update(
+        "insert into Ingredient (id, name, type) values (?, ?, ?)",
+        ingredient.getId(),
+        ingredient.getName(),
+        ingredient.getType().toString());
+    return ingredient;
   }
 }
