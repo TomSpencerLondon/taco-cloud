bdde2d4c018e6b88aba502ad17f9f17df5c41027
Tom <thomas.spencer@codurance.com>
2020-06-11 19:44:59 +0100

    Add sessionStatus to processOrder

--- src/main/java/com/codurance/tacocloud/tacos/OrderController.java
+++ src/main/java/com/codurance/tacocloud/tacos/OrderController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
 @Controller
@@ -25,16 +26,17 @@ public class OrderController {
 
   @GetMapping("/current")
   public String orderForm(Model model){
-    model.addAttribute("order", new Order());
     return "orderForm";
   }
 
   @PostMapping
-  public String processOrder(@Valid Order order, Errors errors){
+  public String processOrder(@Valid Order order, Errors errors,
+      SessionStatus sessionStatus){
     if (errors.hasErrors()){
       return "orderForm";
     }
-    log.info("Order submitted: " + order);
+    orderRepo.save(order);
+    sessionStatus.setComplete();
     return "redirect:/";
   }
 }
